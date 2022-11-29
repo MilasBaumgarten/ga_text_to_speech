09a38d9234cb52b875e291c48ef6f7233e8f8702
Milas Baumgarten <milas.baumgarten@freenet.de>
2022-11-29 14:27:10 +0100

    change to gtts

--- Readme.md
+++ Readme.md
@@ -5,19 +5,36 @@
 	- restart the editor after adding the extension
 - install the python pyttsx3 plugin
 	- run ```addons\pythonscript\windows-64\python.exe -m ensurepip```
-	- run ```addons\pythonscript\windows-64\python.exe -m pip install pyttsx3```
+	- run ```addons\pythonscript\windows-64\python.exe -m pip install pygame```
+	- run ```addons\pythonscript\windows-64\python.exe -m pip install gtts```
 
 ## Usage
-Text to speech can be easily added using the pyttsx3 plugin.
+Text to speech can be easily added using the gTTS plugin. As gTTS intends to save the audio to a file, it is recommended, to use e.g. BytesIO to store the audio in memory. For the example, pygame is used to play the audio from memory.
+
 ```py
-import pyttsx3
+from gtts import gTTS
+import pygame
+from io import BytesIO
 
 
-# initialisation
-engine = pyttsx3.init()
+pygame.init()
+pygame.mixer.init()
 
-# testing
-engine.say('My first code on text-to-speech')
-engine.say('Thank you, Geeksforgeeks')
-engine.runAndWait()
+
+def generate_tts(text):
+    mp3_fp = BytesIO()
+    tts = gTTS(text, lang='en', slow=False)
+    tts.write_to_fp(mp3_fp)
+    return mp3_fp
+
+
+def play_tts(sound):
+    pygame.mixer.music.stop()
+    sound.seek(0)
+    pygame.mixer.music.load(sound, "mp3")
+    pygame.mixer.music.play()
+
+
+sound = generate_tts("Hello World!")
+play_tts(sound)
 ```
--- button_description.py
+++ button_description.py
@@ -1,31 +1,38 @@
 from godot import exposed, export
 from godot import *
 
-import pyttsx3
-import threading
+from gtts import gTTS
+import pygame
+from io import BytesIO
 
 
 @exposed
 class Button_Description(Button):
-    engine = pyttsx3.init()
-
     def _ready(self):
-        
         # connect signal
         self.connect("focus_entered", self, "_on_focus_entered")
+        
+        # setup pygame audio
+        pygame.init()
+        pygame.mixer.init()
+
 
     def _on_focus_entered(self):
         print("entered: " + str(self.text))
         
-        threading.Thread(
-            target=self._say_text_async, args=(self.text,), daemon=True
-        ).start()
-    
-    def _say_text_async(self, text):
-        if self.engine.isBusy():
-            self.engine.stop()
-#            self.engine.endLoop()
-        
-        self.engine.say(text)
-#        self.engine.runAndWait()
-        self.engine.startLoop()
+        sound = self._generate_tts(str(self.text))
+        self._play_tts(sound)
+
+
+    def _generate_tts(self, text):
+        mp3_fp = BytesIO()
+        tts = gTTS(text, lang='en', tld='co.uk', slow=False)
+        tts.write_to_fp(mp3_fp)
+        return mp3_fp
+
+
+    def _play_tts(self, sound):
+        pygame.mixer.music.stop()
+        sound.seek(0)
+        pygame.mixer.music.load(sound, "mp3")
+        pygame.mixer.music.play()
